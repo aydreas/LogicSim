366c87f99017b76fe0bfd6cc5defe25038f4c29a
EinFallsLos <andreas.sch4@gmail.com>
2019-01-28 10:41:36 +0100

    added TODO list; some js changes for better testing

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 const logicsim = require('./build/Release/logicsim.node');
+
+var components = [];
+
+components.push({
+    "type": "CLK",
+    "CLK_Speed": 1,
+    "inputs": [
+        1
+    ],
+    "outputs": [
+        0
+    ]
+});
+
+for (let i = 0; i < 1; i++) {
+    components.push({
+        "type": "AND",
+        "inputs": [
+            0, 1
+        ],
+        "outputs": [
+            2
+        ]
+    });
+}
+
 logicsim.init({
 	"links" : 3,
-	"threads": 1,
-	"components" : [
-		{
-			"type" : "CLK",
-			"CLK_Speed" : 1,
-			"inputs" : [
-				1
-			],
-			"outputs" : [
-				0
-			]
-		},
-		{
-			"type" : "AND",
-			"inputs" : [
-				0, 1
-			],
-			"outputs" : [
-				2
-			]
-		},
-		{
-			"type" : "AND",
-			"inputs" : [
-				0, 1
-			],
-			"outputs" : [
-				2
-			]
-		}
-	]
+    "threads": 1,
+    "components": components
 });
 logicsim.start();
 
