872d990ae2d92123557bdc5d178c34fc91a213bc
EinFallsLos <andreas.sch4@gmail.com>
2019-01-28 10:50:49 +0100

    branch push

--- src/project.cpp
+++ src/project.cpp
@@ -10,6 +10,7 @@
 #include "clk.h"
 #include "node.h"
 
+//TODO: instatiable
 void init(const Nan::FunctionCallbackInfo<v8::Value>& args) {
 	if (args.Length() != 1 || !args[0]->IsObject()) {
 		Nan::ThrowTypeError("Error: One object expected!");
