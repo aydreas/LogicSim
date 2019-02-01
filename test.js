d94bf3af5c9fa2f1aa27444a9977ce2038e9fc5a
Andreas Schlößl <andreas.sch4@gmail.com>
2019-02-01 12:34:30 +0100

    fixed path in test script

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ run();
 
 async function run() {
     for (let fileIndex = 2; fileIndex < args.length; fileIndex++) {
-        let config = JSON.parse(fs.readFileSync("./benchmarks/" + args[2]));
+        let config = JSON.parse(fs.readFileSync("./tests/" + args[2]));
         logicsim.newBoard("testBoard_" + fileIndex, config.board);
         logicsim.startBoard("testBoard_" + fileIndex);
 
@@ -17,7 +17,7 @@ async function run() {
         await new Promise(resolve => setTimeout(resolve, config.simulationTime));
         logicsim.stopBoard("testBoard_" + fileIndex);
         let boardState = logicsim.getBoard("testBoard_" + fileIndex);
-
+		console.log(boardState);
         for (let i = 0; i < config.expected.components.length; i++) {
             for (let j = 0; j < config.expected.components[i].length; j++) {
                 if (boardState.components[i][j] !== config.expected.components[i][j])
